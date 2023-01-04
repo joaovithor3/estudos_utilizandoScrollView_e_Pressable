6caebd0a727bc513773b2d828c9d312ea7024439
joaovithor3 <121798550+joaovithor3@users.noreply.github.com>
2023-01-04 12:37:23 -0300

    App Sobre planetas, aprendendo a usar ScrollView e Pressable

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import TelaInicial from '../ScrollView/Tela Inicial/index';
+import Planetas from '../ScrollView/Paginas/Planetas/index';
+import Galaxias from '../ScrollView/Paginas/Galaxias/index';
+import Buraco from '../ScrollView/Paginas/Buraco Negro/index';
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ <NavigationContainer>
+  <Stack.Navigator>
+  <Stack.Screen name= 'Tela Inicial' component ={TelaInicial} />
+  <Stack.Screen name= 'Planetas' component ={Planetas} />
+  <Stack.Screen name= 'Galáxias' component ={Galaxias} />
+  <Stack.Screen name= 'Buraco Negro' component ={Buraco}/>
+  </Stack.Navigator>
+ </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
